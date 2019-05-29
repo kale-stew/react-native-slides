6ac0bf6cc36bf2a000d29164b940044cf40c948c
Kylie Stewart <kylieastewart@gmail.com>
2019-05-28 18:29:43 -0600

    Updates

---
+++ precache-manifest.bae45d29ba947e55180051774db42e8d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d785308de1c8be656bd05cced037bce5",
+    "url": "/react-to-react-native/index.html"
+  },
+  {
+    "revision": "d60d05c4390b2d20f984",
+    "url": "/react-to-react-native/static/css/main.7909de89.chunk.css"
+  },
+  {
+    "revision": "c0f42d4bff01734704a5",
+    "url": "/react-to-react-native/static/js/2.f23b8af0.chunk.js"
+  },
+  {
+    "revision": "d60d05c4390b2d20f984",
+    "url": "/react-to-react-native/static/js/main.2b8ec7a6.chunk.js"
+  },
+  {
+    "revision": "8491ff1a72f5848838f3",
+    "url": "/react-to-react-native/static/js/runtime~main.6ab595c0.js"
+  },
+  {
+    "revision": "622b9dde94cac479d122af5f849a726e",
+    "url": "/react-to-react-native/static/media/copy-paste.622b9dde.gif"
+  },
+  {
+    "revision": "c91b9e7cc7d2dd5fb94ff99035681dc0",
+    "url": "/react-to-react-native/static/media/in-browser.c91b9e7c.png"
+  },
+  {
+    "revision": "1e1c04bebf0396da3c9817fb464ce545",
+    "url": "/react-to-react-native/static/media/in-react-native.1e1c04be.png"
+  },
+  {
+    "revision": "8cae3f23bacca2e6f5fc37949ef6db45",
+    "url": "/react-to-react-native/static/media/new-formidable.8cae3f23.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/react-to-react-native/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "91d54853d48d3e32f4c663b94b2a16bb",
+    "url": "/react-to-react-native/static/media/roadmap.91d54853.png"
+  },
+  {
+    "revision": "bb4aee5d51d7f5731ee4260827502b32",
+    "url": "/react-to-react-native/static/media/stack-navigator.bb4aee5d.gif"
+  },
+  {
+    "revision": "38258a7af317e501414c962a15f7533b",
+    "url": "/react-to-react-native/static/media/tab-navigator.38258a7a.gif"
+  },
+  {
+    "revision": "099c0502bd12fbf968daa37405b64a3a",
+    "url": "/react-to-react-native/static/media/todo.099c0502.gif"
+  }
+]);
