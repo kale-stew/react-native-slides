49268c681f334c95d02ac7e8bb689cd5d8412e41
Kylie Stewart <kylieastewart@gmail.com>
2019-11-15 15:40:39 -0500

    RVA.js

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
-# React to React Native: How Hard Could It Be?
+# From React to React Native: How Hard Could It Be?
 
-These are the slides from my talk at [DenverScript](https://www.meetup.com/DenverScript/events/cswxqqyzhblc/) on May 28th, 2019.
+These are the slides from my presentation at [RVA.js](https://www.rvajavascript.com/) in Richmond, Virginia on November 15th, 2019, titled "React to React Native: How Hard Could It Be?".
 
-In this talk, I cover the basics of React and how the browser framework differs from React Native.
+In this talk, I answer the following questions:
+
+- What is React?
+- What is React Native?
+- How does React differ from React Native?
+- What are some pain points a dev might encounter transitioning a React app to React Native?
+- What's in store for the future of React Native development?
 
 ## Just Looking for Slides?
 
-You could check out this [PDF of my slides](https://github.com/kale-stew/react-to-react-native/blob/master/final.pdf).
+You could check out
 
-Or maybe even the [live view](https://kale-stew.github.io/react-to-react-native/).
+1. This [exported PDF of the slides themselves](./final.pdf)
+2. The [live view](https://kyliestewart.tech/react-native-slides/)
+
+<!-- 3. My [blog post about this talk](https://kyliestewart.tech/blog/react-native) -->
 
 ## Further Reading
 
-Some good places to get started with React Native:
-
 - https://facebook.github.io/react-native/docs/tutorial
+- https://medium.com/swlh/react-native-a-bridge-to-project-fabric-part-1-5af6a53c0d83
 
 ## Acknowledgements
 
-Thank you for letting me speak, [DenverScript](https://www.meetup.com/DenverScript)!
+Thank you for having me, RVA.js! I always appreciate the opportunity to visit my college town. 🙂 Go Rams!
--- src/presentation.js
+++ src/presentation.js
@@ -1,62 +1,54 @@
-import React, { Component } from 'react';
-import './fonts.css';
+import React, { Component } from "react";
+import "./fonts.css";
 
-import * as CodeSamples from './code-samples';
-
-// spectacle-core tags
 import {
   Appear,
   Code,
-  CodePane,
   Deck,
   Fill,
   Heading,
   Image,
   Layout,
-  Notes,
   Slide,
   Text
-} from 'spectacle';
+} from "spectacle";
 
-// theme
-import createTheme from 'spectacle/lib/themes/default';
+import createTheme from "spectacle/lib/themes/default";
 const theme = createTheme(
   {
-    black: '#272727',
-    teal: '#30A7DB',
-    navy: '#091540',
-    offWhite: '#EFF4F9',
-    green: '#26C145',
-    orange: '#F04D20',
-    primary: '#EFEFEF',
-    white: '#fff'
+    black: "#272727",
+    teal: "#30A7DB",
+    navy: "#091540",
+    offWhite: "#EFF4F9",
+    green: "#26C145",
+    orange: "#F04D20",
+    primary: "#EFEFEF",
+    white: "#fff"
   },
   {
-    primary: 'Fira Sans',
-    secondary: 'Montserrat',
-    tertiary: 'Space Mono'
+    primary: "Fira Sans",
+    secondary: "Montserrat",
+    tertiary: "Space Mono"
   }
 );
 
 // image assets
-/* eslint-disable no-undef */
 const images = {
-  BrowserSample: require('./assets/in-browser.png'),
-  CopyPaste: require('./assets/copy-paste.gif'),
-  FormidaLogo: require('./assets/new-formidable.png'),
-  NativeSample: require('./assets/in-react-native.png'),
-  React: require('./assets/react.png'),
-  Roadmap: require('./assets/roadmap.png'),
-  SimpleTodo: require('./assets/todo.gif'),
-  StackNav: require('./assets/stack-navigator.gif'),
-  TabNav: require('./assets/tab-navigator.gif')
+  CreateNative: require("./assets/create-react-native.png"),
+  CreateReact: require("./assets/create-react-app.png"),
+  FormidaLogo: require("./assets/new-formidable.png"),
+  PlatformCode: require("./assets/platform-specific.png"),
+  Questions: require("./assets/questions.png"),
+  React: require("./assets/react.png"),
+  ReactNative: require("./assets/react-native.png"),
+  Roadmap: require("./assets/roadmap.png"),
+  Wave: require("./assets/wave.png")
 };
-/* eslint-enable no-undef */
 
 export default class Presentation extends Component {
   render() {
     return (
-      <Deck transition={['slide']} theme={theme}>
+      <Deck transition={["slide"]} theme={theme}>
         <Slide bgColor="teal">
           <Heading textColor="offWhite" fit={true}>
             React to React Native:
@@ -74,31 +66,46 @@ export default class Presentation extends Component {
           <Heading textFont="Montserrat" textColor="navy" size={6}>
             Software Engineer at Formidable
           </Heading>
-          <Notes>
-            My goal is:
-            <ul>
-              <li>to introduce you to React, if you're unfamiliar</li>
-              <li>something else i think</li>
-            </ul>
-          </Notes>
         </Slide>
 
-        {/** WHAT IS REACT? */}
+        <Slide bgColor="teal">
+          <Layout>
+            <Fill>
+              <Image src={images.Wave} width="330px" />
+            </Fill>
+            <Fill>
+              <Heading fit={true} textColor="offWhite">
+                Kylie Stewart
+              </Heading>
+              <br />
+              <Heading size={4} textColor="offWhite">
+                Software Engineer
+              </Heading>
+              <Heading size={4} textColor="offWhite">
+                at Formidable
+              </Heading>
+            </Fill>
+          </Layout>
+        </Slide>
+
+        {/** WHAT IS REACT */}
         <Slide bgColor="teal">
           <Heading size={3} textColor="offWhite">
             What is React?
           </Heading>
           <br />
           <Image src={images.React} width="330px" />
-          <Notes>javascript library for building user interfaces</Notes>
         </Slide>
 
-        {/** WHY?? */}
+        {/** WHY REACT */}
         <Slide bgColor="teal">
           <Heading size={4} textColor="offWhite">
             But why use it?
           </Heading>
           <br />
+          <Appear>
+            <Text textColor="offWhite">open source</Text>
+          </Appear>
           <Appear>
             <Text textColor="offWhite">JSX</Text>
           </Appear>
@@ -108,196 +115,189 @@ export default class Presentation extends Component {
           <Appear>
             <Text textColor="offWhite">component interfaces</Text>
           </Appear>
-          <Notes>
-            it's just JS! the DOM is hard, component interfaces are cool
-          </Notes>
         </Slide>
 
         {/** WHAT IS REACT NATIVE */}
         <Slide bgColor="teal">
           <Heading fit={true}>What is React Native?</Heading>
+          <br />
+          <Image src={images.ReactNative} height="330px" />
+        </Slide>
+
+        {/** WHY REACT NATIVE */}
+        <Slide bgColor="teal">
+          <Heading size={4} textColor="offWhite">
+            The dream 🙌
+          </Heading>
+          <br />
+          <Appear>
+            <Heading textColor="offWhite">
+              <i>Build once,</i>
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading textColor="offWhite">
+              <i>deploy anywhere</i>
+            </Heading>
+          </Appear>
         </Slide>
 
         {/** CHALLENGES */}
         <Slide bgColor="navy">
-          <Heading>Our Starting Point</Heading>
+          <Heading fit={true}>How hard could it be?</Heading>
           <br />
-          <Image src={images.SimpleTodo} width="550px" />
+          <Image
+            src={
+              "https://media0.giphy.com/media/xT0xeuOy2Fcl9vDGiA/giphy.gif?cid=790b76110b82aa6e243f8984f528c0811b36b543f9d9e2e0&rid=giphy.gif"
+            }
+          />
         </Slide>
 
-        <Slide bgColor="navy">
-          <Heading fit={true}>"Can I just copy and paste the code?"</Heading>
+        <Slide bgColor="green">
+          <Heading fit={true}>Getting Started with React</Heading>
           <br />
-          <Image src={images.CopyPaste} width="350px" />
-          <br />
+          <Image src={images.CreateReact} height="150px" />
         </Slide>
 
-        {/** #1: STRUCTURE */}
+        <Slide bgColor="green">
+          <Heading fit={true}>Getting Started with React Native</Heading>
+          <br />
+          <Image src={images.CreateNative} height="150px" />
+        </Slide>
+
         <Slide bgColor="orange">
           <Heading fit={true} textColor="offWhite">
-            Challenge #1: API 🔨
+            Challenge #1: Syntax 📚
           </Heading>
           <br />
-          <Layout>
-            <Fill>
-              <Appear>
-                <CodePane
-                  source={CodeSamples.TodoBeforeNative}
-                  color="white"
-                  theme="light"
-                />
-              </Appear>
-            </Fill>
-            <Fill>
-              <Appear>
-                <CodePane
-                  source={CodeSamples.TodoAfterNative}
-                  color="white"
-                  theme="light"
-                />
-              </Appear>
-            </Fill>
-          </Layout>
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              d̶i̶v̶ View
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              b̶u̶t̶t̶o̶n̶s̶ touchables
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              o̶n̶C̶l̶i̶c̶k̶ onPress
+            </Heading>
+          </Appear>
         </Slide>
 
-        {/** #2: STYLES */}
+        {/** STYLES  */}
         <Slide bgColor="green">
-          <Heading>Challenge #2: Styling 💅🏻</Heading>
-        </Slide>
-
-        <Slide bgColor="green">
-          <Heading textColor="offWhite" size={4}>
-            Before
+          <Heading fit={true} textColor="offWhite">
+            Challenge #2: Styles 💅🏻
           </Heading>
           <br />
-          <Image src={images.BrowserSample} />
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              inline styles
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              flexbox everything
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              font trickery
+            </Heading>
+          </Appear>
         </Slide>
 
-        <Slide bgColor="green">
-          <Heading textColor="offWhite" size={4}>
-            Before
-          </Heading>
-          <br />
-          <Layout>
-            <Fill>
-              <CodePane
-                source={CodeSamples.AppJSSnippet}
-                color="white"
-                theme="light"
-              />
-            </Fill>
-            <Fill>
-              <CodePane
-                source={CodeSamples.AppCSS}
-                color="white"
-                theme="light"
-              />
-            </Fill>
-          </Layout>
-        </Slide>
-
-        <Slide bgColor="green">
-          <Heading textColor="offWhite" size={4}>
-            After
-          </Heading>
-          <br />
-          <Layout>
-            <Fill>
-              <Image src={images.NativeSample} width="280px" />
-            </Fill>
-            <Fill>
-              <CodePane
-                source={CodeSamples.Native}
-                color="white"
-                theme="light"
-              />
-            </Fill>
-          </Layout>
-        </Slide>
-
-        {/** #3: NAVIGATION */}
-        <Slide bgColor="navy">
-          <Heading>Challenge #3: Navigation 📍</Heading>
-        </Slide>
-
-        <Slide bgColor="navy">
-          <Heading textColor="offWhite" size={4}>
-            Tab Navigator
-          </Heading>
-          <br />
-          <Layout>
-            <Fill>
-              <Image src={images.TabNav} width="330px" />
-            </Fill>
-            <Fill>
-              <CodePane
-                source={CodeSamples.TabNavigator}
-                color="white"
-                theme="light"
-              />
-            </Fill>
-          </Layout>
-        </Slide>
-
-        <Slide bgColor="navy">
-          <Heading textColor="offWhite" size={4}>
-            Stack Navigator
-          </Heading>
-          <br />
-          <Layout>
-            <Fill>
-              <Image src={images.StackNav} width="330px" />
-            </Fill>
-            <Fill>
-              <CodePane
-                source={CodeSamples.StackNavigator}
-                color="white"
-                theme="light"
-              />
-            </Fill>
-          </Layout>
-        </Slide>
-
-        {/** #5: ENVIRONMENT */}
+        {/** INFRASTRUCTURE */}
         <Slide bgColor="teal">
-          <Heading>Challenge #4: Environment 🏠</Heading>
+          <Heading fit={true} textColor="offWhite">
+            Challenge #3: Infrastructure 🛠
+          </Heading>
+          <br />
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              l̶o̶c̶a̶l̶S̶t̶o̶r̶a̶g̶e̶ AsyncStorage
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              offline accessibility
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              ... and don't forget about the app store!
+            </Heading>
+          </Appear>
         </Slide>
 
-        <Slide bgColor="teal">
-          <Appear>
-            <Text textColor="offWhite">Before</Text>
-          </Appear>
-          <Appear>
-            <CodePane
-              source={CodeSamples.EnvBefore}
-              color="white"
-              theme="light"
-            />
-          </Appear>
+        {/** NATIVE BINDINGS */}
+        <Slide bgColor="navy">
+          <Heading fit={true} textColor="offWhite">
+            Challenge #4: Native Bindings 📲
+          </Heading>
           <br />
-
           <Appear>
-            <Text textColor="offWhite">After</Text>
-          </Appear>
-          <Appear>
-            <CodePane
-              source={CodeSamples.EnvAfter}
-              color="white"
-              theme="light"
-            />
+            <Image src={images.PlatformCode} height="450px" />
           </Appear>
         </Slide>
 
         {/** THE FUTURE */}
         <Slide bgColor="green">
-          <Heading size={4}>✨ The Future ✨</Heading>
+          <Heading size={4} textColor="offWhite">
+            ✨ The Future ✨
+          </Heading>
           <br />
-          <Image src={images.Roadmap} height="400px" />
           <br />
-          <Code style={{ color: 'white' }}>
-            https://github.com/facebook/react-native/wiki/Roadmap
-          </Code>
-          <Notes>no more broken versions!</Notes>
+          <Appear>
+            <Heading size={1} textColor="offWhite">
+              <i>Fabric</i>
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgColor="green">
+          <Heading size={2} textColor="offWhite">
+            Fabric
+          </Heading>
+          <br />
+          <br />
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              new threading model
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              async rendering
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading size={4} textColor="offWhite">
+              simplified native bridge
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgColor="green">
+          <Image src={images.Roadmap} />
+        </Slide>
+
+        <Slide bgColor="orange">
+          <Layout>
+            <Fill>
+              <Image src={images.Questions} width="330px" />
+            </Fill>
+            <Fill>
+              <br />
+              <br />
+              <Heading size={2} textColor="offWhite">
+                Any questions?
+              </Heading>
+            </Fill>
+          </Layout>
         </Slide>
 
         {/** FIN */}
@@ -309,8 +309,8 @@ export default class Presentation extends Component {
             @KYLIESTEW ON TWITTER
           </Heading>
           <br />
-          <Code style={{ color: 'white' }}>
-            github.com/kale-stew/ts-in-react
+          <Code style={{ color: "white" }}>
+            kyliestewart.tech/react-native-slides
           </Code>
         </Slide>
       </Deck>
